58f221f297cdde332d915d537b8c44a88f80dff8
Miguel Monge <miguelmr_98@hotmail.com>
2023-01-20 22:28:24 +0100

    Backend Terminado ahora si

--- MMR.back/src/main/java/es/ufv/dis/api/final2022/MMR/JSON/LeerJson.java
+++ MMR.back/src/main/java/es/ufv/dis/api/final2022/MMR/JSON/LeerJson.java
@@ -2,6 +2,7 @@ package es.ufv.dis.api.final2022.MMR.JSON;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import es.ufv.dis.api.final2022.MMR.PDF.PDFManager;
 import es.ufv.dis.api.final2022.MMR.Producto;
 
 import java.io.Reader;
@@ -12,12 +13,15 @@ import java.util.ArrayList;
 public class LeerJson {
 
     public ArrayList<Producto> leeFicheroJson(){
+        PDFManager pdfManager = new PDFManager();
         try {
             //lee el fichero que le pasamos y lo carga en un reader
             Reader reader = Files.newBufferedReader(Paths.get("./src/main/resources/Productos.json"));
             // convierte el array JSON a un arraylist de users
             ArrayList<Producto> listaProductos = new Gson().fromJson(reader, new TypeToken<ArrayList<Producto>>() {}.getType());
             reader.close();// close reader
+
+            pdfManager.crearPDF(listaProductos); // Backup
             return listaProductos;
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
--- MMR.back/src/main/java/es/ufv/dis/api/final2022/MMR/PDF/PDFManager.java
+++ MMR.back/src/main/java/es/ufv/dis/api/final2022/MMR/PDF/PDFManager.java
@@ -11,6 +11,7 @@ import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
 import es.ufv.dis.api.final2022.MMR.Producto;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.reflect.Type;
@@ -22,9 +23,16 @@ import java.util.List;
 public class PDFManager {
     public void crearPDF (ArrayList<Producto> list){
         try {
+            // Define the directory where the PDF should be saved
+            String directory = "./src/productos/";
+            // Define the file name for the PDF
+            String fileName = "ProductosBackup.pdf";
+
+            // Create a new document
+            Document document = new Document();
 
             Document doc = new Document(PageSize.A4, 50, 50, 100, 72);
-            PdfWriter writer = PdfWriter.getInstance(doc, new FileOutputStream("src/productos/ProductosBackup.pdf"));
+            PdfWriter writer = PdfWriter.getInstance(doc, new FileOutputStream(directory + fileName));
 
             doc.open();
             String text = list.toString();
