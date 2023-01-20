aafa36bcddf98abc557d870a2529e75f2f56e6fc
Miguel Monge <miguelmr_98@hotmail.com>
2023-01-20 21:05:02 +0100

    Proyects Setup

---
+++ MMR.back/src/test/java/es/ufv/dis/api/final2022/MMR/MmrApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ufv.dis.api.final2022.MMR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
